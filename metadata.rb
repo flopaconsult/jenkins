27ed5a3ebcef4eb50f6f40963652c4f14388d99f
Florin Popa <flopaconsult@gmail.com>
2013-04-18 23:13:26 +0300

    changes from florin-wrk on Thu Apr 18 23:13:26 EEST 2013

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,6 @@ description      "Installs and configures Jenkins CI server & slaves"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.6.3"
 
-%w(runit,'~> 1.0.0' java iptables).each { |cb| depends cb }
+depends 'runit','~> 1.0.0'
+%w(java iptables).each { |cb| depends cb }
 %w(iptables yum apt).each { |cb| recommends cb }
